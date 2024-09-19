efc6ac47e6e5a7626e130d3eaffd8bea21817337
Simon Haines <nimon@hotmail.co.uk>
2024-09-19 03:52:23 +0100

    Update Exercise3.java

--- src/main/java/com/corndel/pixmate/exercises/Exercise3.java
+++ src/main/java/com/corndel/pixmate/exercises/Exercise3.java
@@ -1,14 +1,18 @@
 package com.corndel.pixmate.exercises;
 
+/**
+ * @see <a href="https://tech-docs.corndel.com/java/nested-loops.html">Nested Loops in Java</a>
+ */
 public class Exercise3 {
-  // https://tech-docs.corndel.com/java/nested-loops.html
 
   /**
    * Given a grid of numbers (an array of arrays), count how many are larger than 10.
-   *
-   * @example // returns 4 gridLimit([ [3, 12, 1], [19, 10, 11], [9, 5, 105] ])
-   * @param {int[][]} grid - an array of arrays of numbers
-   * @returns {int} The count of numbers larger than 10
+   * <p>
+   * For example, if the input grid is [ [3, 12, 1], [19, 10, 11], [9, 5, 105] ], the method returns 4.
+   * </p>
+   * 
+   * @param grid an array of arrays of numbers
+   * @return the count of numbers larger than 10
    */
   public static int gridLimit(int[][] grid) {
     // TODO
