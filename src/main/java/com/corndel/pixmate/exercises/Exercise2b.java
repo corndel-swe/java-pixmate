f5ae077679db5dd6389e5918d29a982f802085db
Simon Haines <nimon@hotmail.co.uk>
2024-09-19 03:51:34 +0100

    Update Exercise2b.java

--- src/main/java/com/corndel/pixmate/exercises/Exercise2b.java
+++ src/main/java/com/corndel/pixmate/exercises/Exercise2b.java
@@ -1,16 +1,19 @@
 package com.corndel.pixmate.exercises;
 
+/**
+ * @see <a href="https://tech-docs.corndel.com/java/loop-control-flow.html">Loop Control Flow in Java</a>
+ */
 public class Exercise2b {
-  // https://tech-docs.corndel.com/java/loop-control-flow.html
 
   /**
    * Finds the index of the first element in an array that is smaller than the element before it.
-   *
-   * @example // returns 3 stepDown([10, 12, 15, 14, 16, 20])
-   * @example // returns -1 (no element is smaller than its predecessor) stepDown([5, 10, 15, 20])
-   * @param {int[]} numbers - An array of numbers to be examined.
-   * @returns {int} The index of the first element that is smaller than its predecessor. Returns -1
-   *     if no such element is found or if the array is empty.
+   * <p>
+   * For example, if the input array is [10, 12, 15, 14, 16, 20], the method returns 3. 
+   * If no element is smaller than its predecessor, such as in [5, 10, 15, 20], the method returns -1.
+   * </p>
+   * 
+   * @param numbers an array of numbers to be examined
+   * @return the index of the first element that is smaller than its predecessor, or -1 if no such element is found
    */
   public static int stepDown(int[] numbers) {
     // TODO
